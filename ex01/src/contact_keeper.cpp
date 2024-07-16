7a7ecb225a2b9640dd7a247b56938b20888bd3ea
iuryr <iuryroberto@gmail.com>
2024-07-16 15:02:26 -0300

    ex01 - basic loop

---
+++ ex01/src/contact_keeper.cpp
@@ -0,0 +1,27 @@
+#include "contact_keeper.hpp"
+
+int main(void)
+{
+	std::string input;
+
+	while (42)
+	{
+		std::cout << "Enter command (ADD, SEARCH or EXIT):" << std::endl;
+		std::cout << ">> ";
+		std::getline(std::cin, input);
+
+		if (input.compare("ADD") == 0 || input.compare("SEARCH") == 0)
+		{
+			std::cout << input << std::endl;
+		}
+		else if (input.compare("EXIT") == 0)
+		{
+			break;
+		}
+		else
+		{
+			std::cout << "Unrecognized command" << std::endl;
+		}
+	}
+	return 0;
+}
---
+++ ex01/src/include/contact_keeper.hpp
@@ -0,0 +1,6 @@
+#ifndef CONTACT_KEEPER_H
+# define CONTACT_KEEPER_H
+
+#include <iostream> //cin, cout
+
+#endif //CONTACT_KEEPER_H
