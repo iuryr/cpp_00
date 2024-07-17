4b6474c8a15d661cd4f109aa1b4b49a5451b520b
iuryr <iuryroberto@gmail.com>
2024-07-17 20:41:23 -0300

    ex02: default destructor

--- ex02/src/Account.cpp
+++ ex02/src/Account.cpp
@@ -26,8 +26,12 @@ Account::Account(int initial_deposit)
 
 Account::~Account(void)
 {
-	//TODO
-	std::cout << "Object destroyed";
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex;
+	std::cout << ";amount:" << this->_amount;
+	std::cout << ";closed" << std::endl;
+	Account::_nbAccounts--;
+	Account::_totalAmount -= this->_totalAmount;
 }
 
 void	Account::_displayTimestamp(void)
