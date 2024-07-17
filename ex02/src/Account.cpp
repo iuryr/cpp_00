0daef88e5e6dec5a0e6ec10cc15bae14f50266ce
iuryr <iuryroberto@gmail.com>
2024-07-17 20:16:33 -0300

    ex02 - Default constructor OK.
    Most of tests.cpp is commented to allow compilation at this point

---
+++ ex02/src/Account.cpp
@@ -0,0 +1,44 @@
+#include "Account.hpp"
+#include <iostream>
+#include <ctime>
+
+/*initializing static member variables in header files is not advisable because
+if they are initialized in other header files the programmer can't be sure of what
+initialization prevails. In this case, since we only have Account.hpp, it's OK.*/
+int Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit)
+{
+	this->_accountIndex = Account::_nbAccounts;
+	Account::_nbAccounts++;
+	this->_amount = initial_deposit;
+	Account::_totalAmount += this->_amount;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex;
+	std::cout << ";amount:" << this->_amount;
+	std::cout << ";created" << std::endl;
+}
+
+Account::~Account(void)
+{
+	//TODO
+	std::cout << "Object destroyed";
+}
+
+void	Account::_displayTimestamp(void)
+{
+	std::time_t		timer;
+	struct std::tm*	time_info;
+	char buffer[18];
+
+	std::time(&timer);
+	time_info = std::localtime(&timer);
+	std::strftime(buffer, 18, "[%Y%m%d_%H%M%S]", time_info);
+	buffer[17] = '\0';
+	std::cout << buffer;
+}
